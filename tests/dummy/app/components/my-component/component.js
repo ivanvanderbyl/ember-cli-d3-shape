d3e05ae31be5b82c4cb07abfff1be598c5590961
Ivan Vanderbyl <ivanvanderbyl@gmail.com>
2015-12-30 10:32:12 +1100

    Updated example component to draw a donut chart

--- tests/dummy/app/components/my-component/component.js
+++ tests/dummy/app/components/my-component/component.js
@@ -1,15 +1,59 @@
 import Ember from 'ember';
 import layout from './template';
+import { arc, pie } from  'd3-shape';
+import { select } from 'd3-selection';
 
-// import path from  'd3-path/path';
-import {cardinal, cardinalClosed} from  'd3-shape';
-import { line } from 'd3-shape';
-// import {basisClosed} from 'ember-cli-d3/d3-shape/index';
+const { computed } = Ember;
 
 export default Ember.Component.extend({
   layout: layout,
 
+  values: [1, 1, 2, 3, 5, 8, 13, 21],
+
+  width: 400,
+  height: 400,
+
+  outerRadius: computed('height', {
+    get() {
+      return this.get('height') / 2 - 24;
+    }
+  }),
+
+  innerRadius: computed('height', {
+    get() {
+      return this.get('outerRadius') / 2;
+    }
+  }),
+
+  pieFn: function() {
+    return pie().padAngle(5/360);
+  },
+
+  arcFn: function() {
+    return arc()
+      .innerRadius(this.get('innerRadius'))
+      .outerRadius(this.get('outerRadius'));
+  },
+
   didInsertElement() {
-    console.log(line);
+    this.chart = select(this.element.querySelector('svg'));
+    this.draw();
+  },
+
+  draw() {
+    const values = this.get('values');
+    const arcs = this.pieFn()(values);
+    const arc = this.arcFn();
+
+    let plot = this.chart.append("g")
+      .attr("transform", "translate(" + this.get('width') / 2 + "," + this.get('height') / 2 + ")");
+
+    let roundPath = plot.append("g")
+      .attr("class", "paths--round")
+      .selectAll("path")
+        .data(values)
+      .enter().append("path");
+
+    roundPath.data(arcs).attr("d", arc.cornerRadius(5));
   }
 });
