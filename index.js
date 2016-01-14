7b65698d5128590d38b5f67995cb0a606f34f66e
Ivan Vanderbyl <ivanvanderbyl@gmail.com>
2016-01-14 15:56:15 +0800

    Refactored source processing so that we can use UMD builds
    This is pretty much the biggest hack we do. Loader.js as used by Ember doesn't declare itself as a true AMD loader (don't get me started). So we trick it by defining AMD for the UMD modules as declared by our d3 packages. This is implemented as a brocolli filter.

--- index.js
+++ index.js
@@ -7,6 +7,30 @@ var path = require('path');
 var esTranspiler = require('broccoli-babel-transpiler');
 const concat = require('broccoli-concat');
 var packageDependencies = require("./package.json")['dependencies'];
+var Filter = require('broccoli-filter');
+
+function UMDToAMDRewriteFilter(inputNode, packageName, options) {
+  options = options || {};
+  Filter.call(this, inputNode, {
+    annotation: options.annotation
+  });
+  this.packageName = packageName;
+}
+
+UMDToAMDRewriteFilter.prototype = Object.create(Filter.prototype);
+UMDToAMDRewriteFilter.prototype.constructor = UMDToAMDRewriteFilter;
+
+function createAMDHeader() {
+  return "  // ember-cli-d3-primitive: Insert a fake AMD definition so that Ember's loader.js loads this UMD build.\n" +
+         "  define.amd = true";
+}
+
+UMDToAMDRewriteFilter.prototype.processString = function(content) {
+  var lines = content.split(/\n/);
+  var header = createAMDHeader();
+  lines.splice(1, 0, header);
+  return lines.join("\n");
+};
 
 module.exports = {
   isDevelopingAddon: function(){
@@ -16,18 +40,7 @@ module.exports = {
   name: 'd3',
 
   d3Modules: [
-    // 'd3-path',
-    // 'd3-shape',
-    // 'd3-selection',
-    // 'd3-array',
-    // 'd3-scale',
-    // 'd3-interpolate',
-    // 'd3-color',
-    // 'd3-format',
-    // 'd3-time-format',
-    // 'd3-time',
-    // 'd3-ease',
-    // 'd3-collection',
+    // Imported from package.json
     // 'd3-transition', // Disabled until this is ported from d3 properly
   ],
 
@@ -50,6 +63,51 @@ module.exports = {
     });
   },
 
+  compileSourceTree: function(src, packageName) {
+    var srcTree = new Funnel(src, {
+      include: ['src/**/*.js', 'index.js'],
+      destDir: '/' + packageName,
+      annotation: 'Funnel: D3 Source ['+ packageName + ']'
+    });
+
+    return esTranspiler(srcTree, {
+      persist: false,
+
+      stage: 0,
+      moduleIds: true,
+      modules: 'amd',
+
+      comments: true,
+      compact: false,
+      loose: false,
+
+      moduleRoot: packageName,
+
+      getModuleId: function (name) {
+        name = name
+          .replace(/\/index$/, '')
+          .replace(/\/src/, '');
+        return name;
+      },
+
+      resolveModuleSource: function (source, filename) {
+        if (/\.\//.test(source)) {
+          // This is a relative import from within the package
+          var newPath = path.resolve("/", filename, "..", source)
+            .replace(/\/src/, '')
+            .replace(/^\//, '');
+
+          return newPath;
+        }else{
+          // This is *probably* an import for another package.
+          return source;
+        }
+      }
+    }, {
+      annotation: 'Transpile D3 source to AMD ['+ packageName+ ']',
+    });
+  },
+
   treeForVendor: function(tree) {
     var trees = [];
 
@@ -57,64 +115,26 @@ module.exports = {
 
     this.d3Modules.forEach(function(packageName) {
       let importFileBaseName = path.parse(require.resolve(packageName)).name;
-      var d3PathToSrc;
+      var d3PathToSrc, srcTree;
+
       if (importFileBaseName === 'index') {
         d3PathToSrc = require.resolve(packageName).replace(/index\.js$/, '');
+        srcTree = this.compileSourceTree(path.join(d3PathToSrc), packageName);
       }else{
         var packageBuildPath = path.join('build', packageName + '.js');
         d3PathToSrc = require.resolve(packageName).replace(packageBuildPath, '');
+        var tree = new Funnel(d3PathToSrc, {
+          include: [packageBuildPath],
+          destDir: '/' + packageName,
+          annotation: 'Funnel: D3 Source ['+ packageName + ']'
+        });
+
+        srcTree = new UMDToAMDRewriteFilter(tree, packageName);
       }
 
-      let srcTree = new Funnel(path.join(d3PathToSrc), {
-        include: ['src/**/*.js', 'index.js'],
-        destDir: '/' + packageName,
-        annotation: 'Funnel: D3 Source ['+ packageName + ']'
-      });
-
-      var compiledSrcTree = esTranspiler(srcTree, {
-        persist: false,
-
-        stage: 0,
-        moduleIds: true,
-        modules: 'amd',
-
-        comments: true,
-        compact: false,
-        loose: false,
-
-        moduleRoot: packageName,
-
-        // blacklist: [
-        //   'useStrict'
-        // ],
-
-        getModuleId: function (name) {
-          name = name
-            .replace(/\/index$/, '')
-            .replace(/\/src/, '');
-          return name;
-        },
-
-        resolveModuleSource: function (source, filename) {
-          if (/\.\//.test(source)) {
-            // This is a relative import from within the package
-            var newPath = path.resolve("/", filename, "..", source)
-              .replace(/\/src/, '')
-              .replace(/^\//, '');
-
-            return newPath;
-          }else{
-            // This is *probably* an import for another package.
-            return source;
-          }
-        }
-      }, {
-        annotation: 'Transpile D3 source to AMD ['+ packageName+ ']',
-      });
-
-      trees.push(concat(compiledSrcTree, {
+      trees.push(concat(srcTree, {
         inputFiles: [
-          '**/**/*.js'
+          '**/**/*.js',
         ],
         outputFile: '/' + packageName + '/' + packageName + '.js'
       }));
