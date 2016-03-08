04542517124fbc4a24cb67716acae4d6fe27f132
Ivan Vanderbyl <ivanvanderbyl@gmail.com>
2016-03-08 15:48:32 -0800

    Updated dummy app to use mixins

--- tests/dummy/app/components/my-component/component.js
+++ tests/dummy/app/components/my-component/component.js
@@ -1,36 +1,46 @@
 import Ember from 'ember';
 import layout from './template';
 import { select } from 'd3-selection';
+import Area from 'ember-cli-d3-shape/mixins/area';
+import Dimensions from 'ember-cli-d3-shape/mixins/dimensions';
 
 const { computed } = Ember;
 
-export default Ember.Component.extend({
+export default Ember.Component.extend(Area, Dimensions, {
   layout: layout,
 
-  values: [1, 2,3,4],
+  classNames: ['my-visualisation'],
 
-  width: 400,
-  height: 400,
+  values: [1, 2, 3, 4],
 
-  outerRadius: computed('height', {
+  margin: {
+    top: 100, left: 100, right: 100, bottom: 100,
+  },
+
+  outerRadius: computed('area.width', {
     get() {
-      return this.get('height') / 2 - 24;
+      return this.get('area.width') / 2 - 24;
     }
   }),
 
-  innerRadius: computed('height', {
+  innerRadius: computed('outerRadius', {
     get() {
       return this.get('outerRadius') - 32;
     }
   }),
 
   didInsertElement() {
-    this.chart = select(this.element.querySelector('svg'));
+    this.plot = select(this.element.querySelector('svg'));
+  },
+
+  didMeasureDimensions() {
     this.setupPlot();
   },
 
   setupPlot() {
-    this.chart.select('g')
+    if (!this.element) { return; }
+
+    this.plot.select('g')
       .attr('transform', `translate(${ this.get('width') / 2 }, ${ this.get('height') / 2 })`);
   },
 });
