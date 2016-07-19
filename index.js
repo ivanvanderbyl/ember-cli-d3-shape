4651f531d74cdedbb2fcdb3857008cb488a54bad
Ivan Vanderbyl <ivanvanderbyl@gmail.com>
2016-07-19 13:01:41 +0200

    Cleanup fallbacks because fs throws

--- index.js
+++ index.js
@@ -82,13 +82,17 @@ module.exports = {
       // Import existing builds from node d3 packages, which are UMD packaged.
       var packageBuildPath = path.join('build', packageName + '.js');
 
-      d3PathToSrc = path.join(d3PackagePath, 'node_modules', packageName);
+      d3PathToSrc = lookupPackage(packageName);
 
-      if (!fs.statSync(path.join(d3PathToSrc)).isDirectory()) {
-        d3PathToSrc = lookupPackage(packageName);
+      try {
+        fs.statSync(path.join(d3PathToSrc)).isDirectory();
+      } catch(err) {
+        d3PathToSrc = path.join(d3PackagePath, 'node_modules', packageName);
       }
 
-      if (!fs.statSync(path.join(d3PathToSrc, packageBuildPath)).isFile()) {
+      try {
+        fs.statSync(path.join(d3PathToSrc, packageBuildPath)).isFile()
+      } catch(err) {
         console.error('[ERROR] D3 Package (' + packageName + ') is not built as expected, cannot continue. Please report this as a bug.');
         return;
       }
